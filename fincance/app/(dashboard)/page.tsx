08b829a20dc8994c5127d3fa24149b3769df882f
maheshpawar3819 <pawarmahesh3819@gmail.com>
2025-03-14 16:15:00 +0530

    add auth routes and new package for authentication clerk

--- fincance/app/(dashboard)/page.tsx
+++ fincance/app/(dashboard)/page.tsx
@@ -4,6 +4,10 @@ export default function Home() {
   return (
     <div>
       <Button>Click Me</Button>
+      <Button>Click Me</Button>
+      <p>this is an authenticated route</p>
     </div>
-  )
+  );
 }
+
+
